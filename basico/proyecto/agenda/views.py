d3050da6e2a8572930ba5e4c780e833196d3cfa2
Alex Condoy <alexrcc17@hotmail.es>
2016-05-12 09:20:15 -0500

    proyecto hasta la fecha

--- basico/proyecto/agenda/views.py
+++ basico/proyecto/agenda/views.py
@@ -3,12 +3,12 @@ from .models import Contacto
 from .forms import Formulario 
 
 
-def presentacion(request):
-	return render(request,"presentacion.html",{})
-
 def inicio(request):
-	f=Formulario()
+	f=Formulario(request.POST or None)
 	context={
 	"form":f
 	}
 	return render(request,"inicio.html",context)
+
+def presentacion(request):
+	return render(request,"presentacion.html",{})
