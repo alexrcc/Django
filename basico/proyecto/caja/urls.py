9448591302ad30937c11448077e9785aad49ae2d
”Alex <alexrcc17@hotmail.es>
2016-05-19 08:50:47 -0500

    Se crean las urls de crear y modificar dentro de las urls.py de la app caja para poder acceder mediante navegador

---
+++ basico/proyecto/caja/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', caja.views.listar),
+    url(r'^crear/$', 'caja.views.crear'),
+    url(r'^modificar/$', 'caja.views.modificar'),
