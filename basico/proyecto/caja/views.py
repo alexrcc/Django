ab9e1f23cfdfdd9fc590c88302b6f45918c16d80
”Alex <alexrcc17@hotmail.es>
2016-05-24 10:20:05 -0500

    modifcar los datos de cliente

--- basico/proyecto/caja/views.py
+++ basico/proyecto/caja/views.py
@@ -43,4 +43,42 @@ def crear(request):
 	
 
 def modificar(request):
-	return render(request,"modificar.html",{})
+	f=FormularioCliente(request.POST or None)
+	cedula=request.GET['cedula']
+	cliente=Cliente.objects.get(cedula=cedula)
+	
+	context={
+		'mod':'Modificar Cliente: '+cliente.nombres+"/"+cliente.cedula,
+		'form':f,
+	}
+	f.fields['cedula'].initial = cliente.cedula
+	f.fields['nombres'].initial = cliente.nombres
+	f.fields['apellidos'].initial = cliente.apellidos
+	f.fields['correo'].initial = cliente.correo
+	f.fields['telefono'].initial = cliente.telefono
+	f.fields['celular'].initial = cliente.celular
+	f.fields['direccion'].initial = cliente.direccion
+	f.fields['sexo'].initial = cliente.sexo
+	f.fields['estadoCivil'].initial = cliente.estadoCivil
+	f.fields['fechaNacimiento'].initial = cliente.fechaNacimiento
+
+	if f.is_valid():
+			f_data=f.cleaned_data
+			idCaja= CajaAhorros.objects.all()[0]
+			n=f_data.get("nombres")
+			a=f_data.get("apellidos")
+			c=f_data.get("cedula")
+			co=f_data.get("correo")
+			t=f_data.get("telefono")
+			celu=f_data.get("celular")
+			direc=f_data.get("direccion")
+			sex=f_data.get("sexo")
+			ec=f_data.get("estadoCivil")
+			fn=f_data.get("fechaNacimiento")
+
+			cliente.save()
+
+
+
+
+	return render(request,"modificar.html",context)
